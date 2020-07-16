72925881250f321dfe53c9e442e20fb1ade291e9
CaioMMukai <caio.mukai@netshoes.com>
2020-07-16 18:16:13 -0300

    updadete
    updadete

--- Teste_gps.py
+++ Teste_gps.py
@@ -10,18 +10,21 @@ Created on Tue Jul 14 12:31:07 2020
 import pandas as pd
 import googlemaps
 from utils.utils import *
+import json
 
+jsonConn = (json.load(open('Caio - AIzaSyB.json')))
+conect = (jsonConn['google']['local']['pass'])
 estacao = read_csv("data/estacao.csv")
 df=estacao
 
-gmaps_key = googlemaps.Client(key = "")
+gmaps_key = googlemaps.Client(key = Conect)
 
 # df["LAT"] = None
 # df["LON"] = None
 
 for i in range(0, len(df), 1):
     geocode_result = gmaps_key.geocode(df.iat[i, 0])
-    #geocode_result = gmaps_key.geocode("Term. Jabaquara  São Paulo  Brazil")
+    geocode_result = gmaps_key.geocode("Term. Jabaquara  São Paulo  Brazil")
     try:
         lat = geocode_result[0]["geometry"]["location"]["lat"]
         lon = geocode_result[0]["geometry"]["location"]["lng"]
@@ -71,10 +74,60 @@ for i in range(0, 10):
     metro=(lat,lon)
     print(distance.distance(house, metro).km)
 
-# house = (-23.5701325,-46.6573532)
-# metro = (-23.6244547,-46.6379525)
+ house = (-23.5701325,-46.6573532)
+ metro = (-23.6244547,-46.6379525)
 #print(distance.distance(wellington, salamanca).km)
 print(distance.distance(house, metro).km)
 
+# =============================================================================
+# Teste Calculo de distancia Api GOOGLE
+# =============================================================================
+
+import  googlemaps
+import json
+from utils.utils import *
+import pandas as pd
+
+df_house = read_csv("data/Data_set_full.csv")
+df_estacao = read_csv("data/estacao.csv")
+
+jsonConn = (json.load(open('Caio - AIzaSyB.json')))
+conect = (jsonConn['google']['local']['pass'])
+gmaps_key = googlemaps.Client(key = conect)
+
+# house = ['R. Cel. Joaquim Ferreira Lôbo, 305','Alameda Campinas, 708 - Jardim Paulista, São Paulo - SP'
+#          , 'R. Gomes de Carvalho, 1146 - Vila Olímpia, São Paulo - SP'
+#          , 'R. Quatá, 76 - Vila Olímpia, São Paulo - State of São Paulo'
+#          , 'R. Turiassú, 1473 sp']
+
+# metro = ['Marginal Pinheiros, 7522 - Vila Olímpia, São Paulo - SP, 04533-085']
+colunas = ['ID_origen','L_ORIGEM','ID_DESTINO','L_DESTINO', 'DISTANCIA','TEMPO']
+
+df_distancia = pd.DataFrame(columns=colunas)
+
+for index,l_house in df_house.iterrows():
+    for index,l_estacao in df_estacao.iterrows():
+        try:
+            distance_result = gmaps_key.distance_matrix(l_house['Endereço']
+            ,l_estacao['address'], mode='walking')
+            distance_walking = (distance_result['rows'][0]['elements'][0]['distance']['value'])
+            tempo_s_walking = (distance_result['rows'][0]['elements'][0]['duration']['value'])
+        except: 
+            distance_walking = 0
+            tempo_s_walking = 0
+        list_temp = (l_house['ID'], l_house['Endereço'], l_estacao['estaçao']
+                     ,l_estacao['address'],distance_walking,tempo_s_walking)
+        #aux = df_house.loc[df_house['Endereço']==l_house, ['ID', 'Endereço']]
+        #aux['ID_DESTINO'] = 
+        temp = pd.DataFrame([list_temp],columns=colunas)
+        df_distancia = df_distancia.append(temp, ignore_index=True)
+save_as_csv(df_distancia, "data/Todos_as_estaçoes.csv")
+# distance_result = gmaps_key.distance_matrix(house[0],metro, mode='walking')
+# distance_walking = (distance_result['rows'][0]['elements'][0]['distance']['value'])
+# tempo_s_walking = (distance_result['rows'][0]['elements'][0]['duration']['value'])
+# print ("Enderesso de Origem: "+ distance_result['origin_addresses'][0])
+# print ("Enderesso de Destino: "+ distance_result['destination_addresses'][0])
+# print (distance_walking/100 )
+# print (tempo_walking /60)
 
 
